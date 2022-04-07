3cb1089711a6d9a268d4352b296eb68a664923b8
Talha ÖZ <talhaoz123@gmail.com>
2022-04-08 01:44:44 +0300

    Update Constants.kt

--- app/src/main/java/com/talhaoz/martianstalker/util/Constants.kt
+++ app/src/main/java/com/talhaoz/martianstalker/util/Constants.kt
@@ -2,7 +2,7 @@ package com.talhaoz.martianstalker.util
 
 class Constants {
     companion object {
-        const val API_KEY = "8B9IyPiWvN3y35BhSC7DyfhgEZvfLzkjDvUmsPdO"
+        //const val API_KEY = ""
         const val BASE_URL = "https://api.nasa.gov/mars-photos/api/v1/"
         const val CONNECTION_TIMEOUT = 30
         const val CURIOSITY_ROVER="curiosity"
